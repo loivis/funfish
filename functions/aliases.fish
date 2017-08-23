fe39a0c5085539d9676535369ae90aa2f1122fdf
loivis <loivis@live.com>
2017-08-23 16:36:52 +0200

    add aliases and profiles

---
+++ functions/aliases.fish
@@ -0,0 +1,38 @@
+alias hej='fuck'
+alias ssh='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+alias lla='ls -al'
+alias tailf='tail -f'
+alias pwgen='pwgen -1Bc 16'
+
+# docker #
+##########
+alias sqlplus='docker run --rm -it loivis/sqlplus'
+
+# directory #
+#############
+abbr -a -- - 'cd -'
+alias ..="cd .."
+alias ..2="cd ../.."
+alias ..3="cd ../../.."
+alias ..4="cd ../../../.."
+alias ..5="cd ../../../../.."
+
+# git #
+#######
+alias gadd='git pull;git add'
+alias gdiff='git diff -w --ignore-blank-lines'
+alias glog='git log --stat'
+alias gpull='git pull'
+alias gstatus='git status'
+function gcommit
+    git pull
+    git commit -m "$argv[1]"
+    git push
+end
+
+# code #
+########
+alias coding='cd $CODE_BASE'
+alias gogo="mkdir -p $GOPATH && cd $GOPATH"
+alias gopy='cd $CODE_BASE/python/'
+alias gosh='cd $CODE_BASE/shell/'
---
+++ init.fish
@@ -0,0 +1,52 @@
+# variable #
+############
+set -g CODE_BASE "$HOME/code"
+set -g GOPATH "$CODE_BASE/go"
+
+# PATH #
+########
+set -U PATH $GOPATH/bin $PATH
+set -U PATH /usr/local/lib/node_modules $PATH
+
+# common #
+##########
+umask 022
+
+# python #
+##########
+# export PYTHONWARNINGS="default"
+set -g PYTHONWARNINGS "ignore"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
+
+# vim #
+#######
+if [ ! -e ~/.vim/autoload/pathogen.vim ]
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
+    #git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    git clone https://github.com/hashivim/vim-hashicorp-tools.git ~/.vim/bundle/vim-hashicorp-tools
+    git clone https://github.com/ekalinin/Dockerfile.vim.git ~/.vim/bundle/Dockerfile
+    git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+    #pip3 install jedi && git clone https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+end
+
+# source #
+##########
+source $HOME/.config/fish/functions/aliases.fish
+
+################################################################################
+if [ ]
+    echo "those shall NEVER be executed"
+    git config --global core.excludesfile ~/.gitignore_global
+    git config --global user.email "loivis@live.com"
+    git config --global user.name "loivis"
+    git config --global push.default simple
+    # windows
+    # git config --global credential.helper wincred
+    # linux
+    # git config --global credential.helper cache
+    # 1 year = 60*60*24*365
+    # set 10 years
+    # git config --global credential.helper 'cache --timeout=315360000'
+end
