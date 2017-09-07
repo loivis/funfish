4bd2eb16595b142624df377a0df095fc0a68a95e
loivis <loivis@live.com>
2017-09-07 14:55:55 +0200

    add aliases for vscode

--- functions/tingcore.fish
+++ functions/tingcore.fish
@@ -1,3 +1,8 @@
+# paths
 alias tingcore='cd ~/code/tingcore'
 alias tingaws='cd ~/code/tingcore/aws'
 alias tingjenkins='cd ~/code/tingcore/jenkins'
+
+# vscode
+alias codeaws='code ~/code/tingcore/aws'
+alias codejenkins='code ~/code/tingcore/jenkins'
