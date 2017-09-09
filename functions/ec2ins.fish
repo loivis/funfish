a248de8062aa23df0fe6edff76026fb94b51014e
loivis <loivis@live.com>
2017-09-09 11:05:24 +0200

    ec2ins: set identity and filter

--- functions/ec2ins.fish
+++ functions/ec2ins.fish
@@ -1,5 +1,7 @@
-function ec2ins  -d 'list ec2 instances and connect if there is only one'
-    set -l user "ubuntu"
+function ec2ins -d 'list ec2 instances and connect if there is only one'
+    set -l env
+    set -l filter
+    set -l user centos ec2-user
     set -l identity "~/.ssh/id_rsa"
     set -l ssh_options "-o ConnectTimeout=5 -o StrictHostKeyChecking=no"
 
@@ -8,9 +10,30 @@ function ec2ins  -d 'list ec2 instances and connect if there is only one'
         --query 'Reservations[].Instances[].[PrivateIpAddress,InstanceId,Tags[?Key==`Name`].Value[]|[0]]' \
         --output text | sort -k3 | tr A-Z a-z)
 
+    for i in (seq 1 (count $argv))
+        switch $argv[$i]
+            case '-i'
+                set identity $argv[(math $i + 1)]
+            case '-e'
+                set env $argv[(math $i + 1)]
+                set identity "~/.ssh/$env.pem"
+            case '-f'
+                set filter $argv[(math $i + 1)]
+        end
+    end
+
+    if [ -z "$filter" -a -n "$env" ]
+        set filter $env
+    end
+
+    echo "ssh_options:" $ssh_options -i $identity
+
+    echo "filter:" $filter
+
     for ins in $instances
-        if [ (string match -r $argv[1] $ins) ]
+        if [ (string match -r $filter $ins) ]
             echo $ins
         end
     end
+
 end
