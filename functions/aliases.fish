539781af5d80ff10dc42e5dfcd1280ef9f5940bf
loivis <loivis@live.com>
2017-09-11 10:10:21 +0200

    mute docker pull

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -6,7 +6,7 @@ alias pwgen='pwgen -1Bc 16'
 
 # docker #
 ##########
-alias sqlplus='docker pull loivis/sqlplus; docker run --rm -it loivis/sqlplus'
+alias sqlplus='docker pull loivis/sqlplus >/dev/null; docker run --rm -it loivis/sqlplus'
 alias cqlsh='docker pull loivis/cqlsh >/dev/null; and docker run --rm -it loivis/cqlsh --cqlversion=3.4.0'
 
 # directory #
