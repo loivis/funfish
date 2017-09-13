71f7b83a21ef37e72fa138d101d983f952ce1a59
loivis <loivis@live.com>
2017-09-13 09:29:13 +0200

    add function to pull all cloned bitbuckets

---
+++ functions/all_buckets.fish
@@ -0,0 +1,17 @@
+function all_buckets -d "pull all bitbucket repositories"
+    set -l BUCKET_BASE $HOME/code/tingcore
+    set -l projects aws jenkins
+
+    for proj in $projects
+        cd $BUCKET_BASE
+        set -l dirs (find ./$proj -mindepth 2 -maxdepth 2 -type d -name ".git")
+        for dir in $dirs
+            set -l repo (dirname $dir)
+            cd $BUCKET_BASE/$repo
+            echo "### $repo"; echo
+            git pull
+            echo; echo
+        end
+    end
+
+end
