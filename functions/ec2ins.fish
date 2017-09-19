5373822dfd0b1446dcb0bd31e7f0e971dfc78fe4
loivis <loivis@live.com>
2017-09-19 21:00:27 +0200

    ec2ins: set user from ami name

--- functions/ec2ins.fish
+++ functions/ec2ins.fish
@@ -2,13 +2,13 @@ function ec2ins -d 'list ec2 instances and connect if there is only one'
     # set variables
     set -l env
     set -l filter
-    set -l identity "$HOME/.ssh/id_rsa"
+    set -g identity "$HOME/.ssh/id_rsa"
     set -l index
     set -l instances
     set -l list_all 0
     set -l match
-    set -l ssh_options -o ConnectTimeout=3 -o StrictHostKeyChecking=no
-    set -l users centos ec2-user
+    set -g ssh_options -o ConnectTimeout=3 -o StrictHostKeyChecking=no
+    set -g user
 
     # parse arguments
     if [ (count $argv) -eq 0 ]
@@ -16,24 +16,12 @@ function ec2ins -d 'list ec2 instances and connect if there is only one'
     else
         for i in (seq 1 (count $argv))
             switch $argv[$i]
-                case '-i'
-                    string match -r '^(prod|stage|test|central)$' $argv[(math $i + 1)] 1>/dev/null
-                    if [ $status -eq 0 ]
-                        set env $argv[(math $i + 1)]
-                        set identity "$HOME/.ssh/$env.pem"
-                    else
-                        set identity $argv[(math $i + 1)]
-                    end
                 case '-f'
                     set filter $argv[(math $i + 1)]
-                case '-u'
-                    set users $argv[(math $i + 1)]
                 case '-x'
                     set index $argv[(math $i + 1)]
             end
         end
-        set ssh_options $ssh_options -i $identity
-        echo ssh_options: $ssh_options
         echo
     end
 
@@ -45,7 +33,7 @@ function ec2ins -d 'list ec2 instances and connect if there is only one'
     # list/filter instances and connect if only one match
     set instances (aws ec2 describe-instances --region eu-west-1 \
         --filters "Name=instance-state-name,Values=running" \
-        --query 'Reservations[].Instances[].[PrivateIpAddress,InstanceId,LaunchTime,InstanceType,KeyName,Tags[?Key==`Name`].Value[]|[0]]' \
+        --query 'Reservations[].Instances[].[PrivateIpAddress,InstanceId,ImageId,LaunchTime,InstanceType,KeyName,Tags[?Key==`Name`].Value[]|[0]]' \
         --output text | sort -k6 )
     for ins in $instances
         test $list_all -eq 1
@@ -64,16 +52,7 @@ function ec2ins -d 'list ec2 instances and connect if there is only one'
             or echo "no match"
         case '1'
             echo $match
-            set -l host (echo $match | awk '{print $1}')
-            for user in $users
-                ssh $ssh_options $user@$host 'exit' 2>/dev/null
-                if [ $status = 0 ]
-                    echo -e "\n################################################################################\n"
-                    echo -e "connecting to $host as $user"
-                    echo -e "\n################################################################################\n"
-                    ssh $ssh_options $user@$host
-                end
-            end
+            __connect $match[$index]
         case '*'
             echo >/tmp/ec2_instances
             for m in $match
@@ -81,19 +60,35 @@ function ec2ins -d 'list ec2 instances and connect if there is only one'
             end
             column -t /tmp/ec2_instances
             if [ $index ]
-                set -l host (echo $match[$index] | awk '{print $1}')
-                for user in $users
-                    # echo $user $host
-                    ssh $ssh_options $user@$host 'exit' 2>/dev/null
-                    # echo $status
-                    if [ $status = 0 ]
-                        echo -e "\n################################################################################\n"
-                        echo -e "\nconnecting to $host as $user\n"
-                        echo -e "\n################################################################################\n"
-                        ssh $ssh_options $user@$host
-                    end
-                end
+                __connect $match[$index]
             end
     end
+end
 
+function __connect -d "ssh connect to instance"
+    set -l match $argv[1]
+    set -l host (echo $match | awk '{print $1}')
+    set -l instance_id (echo $match | awk '{print $2}')
+    set -l image_id (echo $match | awk '{print $3}')
+    set -g identity $HOME/.ssh/(echo $match | awk '{print $6}').pem
+    set -g ssh_options $ssh_options -i $identity
+    __set_user $image_id
+    if [ $status = 0 ]
+        echo -e "\n################################################################################\n"
+        echo -e "\nconnecting to $host as $user\n"
+        echo -e "\n################################################################################\n"
+        # echo $ssh_options
+        ssh $ssh_options $user@$host
+    end
+end
+
+function __set_user -d "description"
+    set -l image_id $argv[1]
+    set -l image_name (aws ec2 describe-images --image-id $image_id --query 'Images[0].Name' --output text)
+    switch $image_name
+        case '*ecs*'
+            set -g user ec2-user
+        case '*'
+            set -g user centos
+    end
 end
