cd585e676d496d730b03e662dc6ad2921bba8b4e
loivis <loivis@live.com>
2017-09-29 09:51:56 +0200

    add notes for awscli commands

---
+++ functions/awscli.fish
@@ -0,0 +1,28 @@
+function awscli -d "notes for awscli commands"
+echo '
+
+# latest ubuntu 16.04 ami
+aws ec2 describe-images --owners "099720109477" --filters Name="root-device-type",Values="ebs" \
+      Name=architecture,Values=x86_64 Name=name,Values="ubuntu/images/hvm-ssd/ubuntu-xenial-16.04*" \
+      --query "sort_by(Images, &CreationDate)[-3:].[ImageId, CreationDate, Name]" --output=text
+
+aws ec2 describe-images --owners "099720109477" --filters Name="root-device-type",Values="ebs" \
+      Name=architecture,Values=x86_64 Name=name,Values="ubuntu/images/hvm-ssd/ubuntu-xenial-16.04*" \
+      --query "sort_by(Images, &CreationDate)[-1].ImageId" --output=text
+
+# latest centos 7 ami
+aws ec2 describe-images --filter Name="name",Values="CentOS Linux 7 x86_64 HVM EBS 1704*" Name="product-code",Values="aw0evgkw8e5c1q413zgy5pjce" \
+    --query "Images[].[ImageId, CreationDate, Name]" --output text
+
+aws ec2 describe-images --filter Name="name",Values="CentOS Linux 7 x86_64 HVM EBS 1704*" Name="product-code",Values="aw0evgkw8e5c1q413zgy5pjce" \
+    --query "Images[].ImageId" --output text
+
+# latest amazon ecs-optimized ami
+aws ec2 describe-images --owners "amazon" --filter Name="name",Values="*amazon-ecs-optimized" \
+    --query "sort_by(Images, &CreationDate)[-3:].[ImageId, CreationDate, Name]" --output text
+
+aws ec2 describe-images --owners "amazon" --filter Name="name",Values="*amazon-ecs-optimized" \
+    --query "sort_by(Images, &CreationDate)[-1].ImageId" --output text
+
+'
+end
