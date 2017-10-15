82fdfed722b242b344e5fa41b2999bc93439c7c9
loivis <loivis@live.com>
2017-10-15 18:07:26 +0200

    update links to gist raw files

--- functions/profile.fish
+++ functions/profile.fish
@@ -1,9 +1,9 @@
 function profile -d "download profiles from gist"
     echo "download vimrc"
-    set -l vimrc "https://gist.githubusercontent.com/loivis/5f734a1eafe9cc03b1e62a879e422dcf/raw/e409003df973550080cb2dee23485fe47f00d8d5/vimrc"
-    curl -s -o $HOME/.vimrc $vimrc
+    set -l vimrc "https://gist.githubusercontent.com/loivis/5f734a1eafe9cc03b1e62a879e422dcf/raw/vimrc"
+    curl -sL -o $HOME/.vimrc $vimrc
 
     echo "download gitignore_global"
-    set -l gitignore_global "https://gist.githubusercontent.com/loivis/5f734a1eafe9cc03b1e62a879e422dcf/raw/f315325602be16770c775807fd854cab97ee62d3/gitignore_global"
-    curl -s -o $HOME/.gitignore_global $gitignore_global
+    set -l gitignore_global "https://gist.github.com/loivis/5f734a1eafe9cc03b1e62a879e422dcf/raw/gitignore_global"
+    curl -sL -o $HOME/.gitignore_global $gitignore_global
 end
