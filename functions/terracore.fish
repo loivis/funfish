e2a3ee18ae61f007739f28e525b29e162bb2617f
loivis <loivis@live.com>
2017-11-05 20:15:27 +0100

    set TF_PLUGIN_CACHE_DIR

--- functions/terracore.fish
+++ functions/terracore.fish
@@ -1,5 +1,10 @@
 function terracore -d "wrapper for terraform from hashicorp"
     set -g AWS_DEFAULT_REGION "eu-west-1"
+
+    if [ ! -s "$HOME/.terraformrc" ]
+        echo "plugin_cache_dir = \"$HOME/.terraform.d/plugin-cache\"" > $HOME/.terraformrc
+    end
+
     if [ (count $argv) -eq 0 ]
         terraform help
         return
