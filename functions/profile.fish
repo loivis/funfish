baebe331b7d70ec2603bb94dc08bd1346845dec1
loivis <loivis@gmail.com>
2025-11-05 12:27:16 +0100

    Update profile with file backup

--- functions/profile.fish
+++ functions/profile.fish
@@ -1,18 +1,49 @@
 function profile -d "download profiles from gist"
-    echo "download .vimrc"
+    backup $HOME/.vimrc
+    echo "downloading .vimrc"
     set -l vimrc "https://raw.githubusercontent.com/loivis/funfish/master/profiles/.vimrc"
     curl -sL -o $HOME/.vimrc $vimrc
 
-    echo "download vim color scheme: jellybeans"
+    echo "downloading vim color scheme: jellybeans"
     mkdir -p $HOME/.vim/colors
     set -l jellybeans "https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim"
     curl -sL -o $HOME/.vim/colors/jellybeans.vim $jellybeans
 
-    echo "download .gitconfig"
-    set -l gitconfig "https://raw.githubusercontent.com/loivis/funfish/master/profiles/.gitconfig"
-    curl -sL -o $HOME/.gitconfig $gitconfig
+    backup $HOME/.gitconfig
+    echo "downloading .gitconfig"
+    set -l url "https://raw.githubusercontent.com/loivis/funfish/master/profiles/.gitconfig"
+    curl -sL -o $HOME/.gitconfig $url
 
-    echo "download .gitignore"
-    set -l gitignore "https://raw.githubusercontent.com/loivis/funfish/master/profiles/.gitignore"
-    curl -sL -o $HOME/.gitignore $gitignore
+    backup $HOME/.gitignore
+    echo "downloading .gitignore"
+    set -l url "https://raw.githubusercontent.com/loivis/funfish/master/profiles/.gitignore"
+    curl -sL -o $HOME/.gitignore $url
+end
+
+function backup --description "Backup a file by appending current date (YYYYMMDD), skip if file does not exist, or backup already"
+    if test (count $argv) -ne 1
+        echo "Usage: backup <file>" >&2
+        return 1
+    end
+
+    set file $argv[1]
+
+    # Skip if file doesn't exist
+    if not test -e $file
+        return 0
+    end
+
+    # Format: YYYYMMDD
+    set date_str (date "+%Y%m%d")
+    set backup_file "$file.$date_str"
+    set base (basename $backup_file)
+
+    # Skip if backup already exists
+    if test -e $backup_file
+        echo "$base exists already"
+        return 0
+    end
+
+    echo "backup to $base"
+    cp $file $backup_file
 end
